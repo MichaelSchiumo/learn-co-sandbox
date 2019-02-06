d3fedfbc8e0f0c9d41c7219ec6efa9d17fb36661
Michael J. Schiumo <mschiumo18@gmail.com>
2019-02-06 20:43:08 +0000

    test

--- the_big_bang_theory/lib/the_big_bang_theory.rb
+++
@@ -1,6 +0,0 @@
-require "the_big_bang_theory/version"
-
-module TheBigBangTheory
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- the_big_bang_theory/lib/the_big_bang_theory/version.rb
+++
@@ -1,3 +0,0 @@
-module TheBigBangTheory
-  VERSION = "0.1.0"
-end
