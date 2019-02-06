cd6296d4c6924d8b5c2dc3a22b0fd284932966eb
Michael J. Schiumo <mschiumo18@gmail.com>
2019-02-06 19:59:43 +0000

    first commit

---
+++ the_big_bang_theory/lib/the_big_bang_theory.rb
@@ -0,0 +1,6 @@
+require "the_big_bang_theory/version"
+
+module TheBigBangTheory
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ the_big_bang_theory/lib/the_big_bang_theory/version.rb
@@ -0,0 +1,3 @@
+module TheBigBangTheory
+  VERSION = "0.1.0"
+end
