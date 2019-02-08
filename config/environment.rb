65dade35558d417a6ec36d26637120fc75ed3361
Michael J. Schiumo <mschiumo18@gmail.com>
2019-02-08 15:54:36 +0000

    bin setup

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+require_relative '../lib/books/cli'
+require_relative '..lib/books/scraper'
+require_relative '../lib/books/version'
+require_relative '../lib/books/books'
