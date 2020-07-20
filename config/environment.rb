71da3bca019480a072216bdf814232ed244896e0
Michael J. Schiumo <mschiumo18@gmail.com>
2020-07-20 14:41:14 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++
@@ -1,8 +0,0 @@
-require 'pry'
-require 'nokogiri'
-require 'open-uri'
-
-require_relative '../lib/books/cli'
-require_relative '..lib/books/scraper'
-require_relative '../lib/books/version'
-require_relative '../lib/books/books'
